6167e9e1bce6ebe7c9b3ac80f454ebdcdf798bf7
Eric Crosson <esc@ericcrosson.com>
2016-04-15 16:08:22 -0500

    Update archlinux-install.sh
    integrate reporter

--- archlinux-install.sh
+++ archlinux-install.sh
@@ -29,33 +29,32 @@ parted -s /dev/sda mktable msdos
 parted -s /dev/sda mkpart primary 0% 100m
 parted -s /dev/sda mkpart primary 100m 100%
 
-reporter "Making filesystems"
+reporter "Make filesystems"
 mkfs.ext2 /dev/sda1  # /boot
 mkfs.ext4 /dev/sda2 # /
 
-reporter "Setting up /mnt"
+reporter "Set up /mnt"
 mount /dev/sda2 /mnt
 mkdir /mnt/boot
 mount /dev/sda1 /mnt/boot
 
-reporter "Ranking pacman mirrors (take a coffee break if you have slow internet)"
+reporter "Rank pacman mirrors (take a coffee break if you have slow internet)"
 mv /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.orig
 rankmirrors -n 6 /etc/pacman.d/mirrorlist.orig >/etc/pacman.d/mirrorlist
 
-reporter "Installing base packages"
+reporter "Install base packages"
 pacman -Syy
 pacstrap /mnt base base-devel
 
-reporter "Installing system"
+reporter "Install system"
 arch-chroot /mnt pacman --noconfirm -S syslinux
 
-# copy ranked mirrorlist over
+reporter "Install new ranked mirrorlist"
 cp /etc/pacman.d/mirrorlist* /mnt/etc/pacman.d
 
-# generate fstab
+reporter "Generate fstab"
 genfstab -p /mnt >>/mnt/etc/fstab
 
-# todo: enable logging in the chroot
 reporter "Chroot-ing into /mnt"
 arch-chroot /mnt /bin/bash <<END_OF_CHROOT
 
@@ -65,6 +64,18 @@ arch-chroot /mnt /bin/bash <<END_OF_CHROOT
 # todo: parse arguments to set these variables
 
 ##### Bootstrap Variables #####
+
+## Turn comments into literal programming, including output during execution.
+function reporter() {
+    message="$1"
+    shift
+    echo
+    echo "${message}"
+    for (( i=0; i<${#message}; i++ )); do
+        echo -n '-'
+    done
+    echo
+}
 
 # User information
 username=eric
@@ -89,13 +100,13 @@ category_install="${category_internet} ${category_compression} ${category_utils}
 ##### Behavior Variables #####
 git_clone_flags='--recursive --depth 1'
 
-
+reporter "Set hostname to ${hostname}"
 echo "${hostname}" >/etc/hostname
 
-# set initial timezone to America/Chicago
+ reporter "Set initial timezone to America/Chicago"
 ln -s /usr/share/zoneinfo/America/Chicago /etc/localtime
 
-# set initial locale
+reporter "Set locale"
 locale >/etc/locale.conf
 echo "en_US.UTF-8 UTF-8" >>/etc/locale.gen
 echo "en_US ISO-8859-1" >>/etc/locale.gen
@@ -104,19 +115,19 @@ locale-gen
 # todo: make modifications to mkinitcpio.conf
 mkinitcpio -p linux
 
-# install syslinux bootloader
+reporter "Install syslinux bootloader"
 syslinux-install_update -i -a -m
 
-# todo: abstract update functionality -- current scheme doesn't make sense twice
-# Shorten syslinux boot delay
+# todo: abstract update functionality -- currently duplicating code
+reporter "Shorten syslinux boot delay"
 sed 's/Timeout.*/Timeout 5/' < /boot/syslinux/syslinux.cfg > /boot/syslinux/syslinux.cfg.new
 mv /boot/syslinux/syslinux.cfg.new /boot/syslinux/syslinux.cfg
 
-# Update syslinux config with correct root disk
+reporter "Update syslinux config with correct root disk"
 sed 's/root=.*/root=\/dev\/sda2 ro/' < /boot/syslinux/syslinux.cfg > /boot/syslinux/syslinux.cfg.new
 mv /boot/syslinux/syslinux.cfg.new /boot/syslinux/syslinux.cfg
 
-# Install yaourt
+reporter "Install yaourt"
 cat <<EOF >> /etc/pacman.conf
 
 [archlinuxfr]
@@ -125,7 +136,7 @@ Server = http://repo.archlinux.fr/\$arch
 EOF
 pacman --noconfirm -Sy yaourt
 
-# Congure yaourt
+reporter "Congure yaourt"
 cat <<EOF >> /etc/yaourtrc
 
 # esc's customizations
@@ -137,57 +148,56 @@ NOENTER=1
 USECOLOR=1
 EOF
 
-# Install general packages
+reporter "Install general packages"
 for apps in ${categories_install}; do
     yaourt --noconfirm -S ${apps}
 done
 
-# Install python tools
+reporter "Install python tools"
 pacman --noconfirm -S python-setuptools
 easy_install pip
 pip install virtualenv{,wrapper}
 
-# Install pacmatic
+reporter "Install pacmatic"
 yaourt --noconfirm -S pacmatic
 # todo: configure pacmatic
 
-# set root password to "root"
+reporter "Set root password to \"root\""
 echo root:root | chpasswd
 
-# Creating and configuring user ${username}
+reporter "Create user ${username}"
 useradd -rms $(which zsh) ${username}
 for group in power, wheel; do
     gpasswd -a ${username} ${goup}
 done
 
-# Set ${username} password to "${username_passwd}"
+reporter "Set ${username} password to \"${username_passwd}\""
 echo ${username}:${username_passwd} | chpasswd
 
 su ${username} <<END_OF_USER_SHELL
 cd /home/${username}
 
-# Install Oh-My-Zsh
+reporter "Install Oh-My-Zsh"
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-# Install spacemacs
+reporter "Install spacemacs"
 git clone ${git_clone_flags} https://github.com/syl20bnr/spacemacs.git .emacs.d
 rm -rf .emacs.d/private
 
-# Install personal dotfiles
+reporter "Install personal dotfiles"
 git clone ${git_clone_flags} ${dotfiles_repo} ${dotfiles_destination}
 cd ${dotfiles_destination}
 git checkout ${dotfiles_branch}
 
-# Stow personal configs
+reporter "Stow personal configs"
 for app in ${stow_list}; do
     stow ${app}
 done
 
-# Generate user RSA keys
+reporter "Generate user RSA keys"
 ssh-keygen -t rsa -N "" -f ~/.ssh/id_rsa
 END_OF_USER_SHELL
 
-# todo: if school laptop install, clone classes and other workspace repos
 END_OF_CHROOT
 
 # unmount
